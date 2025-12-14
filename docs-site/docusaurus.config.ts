cbd311d4ae3e52c0531c5cb717ec60b2793fb1f4
manjunathh-xyz <144999854+manjunathh-xyz@users.noreply.github.com>
2025-12-14 13:23:32 +0000

    fix: correct Docusaurus url and baseUrl

--- docs-site/docusaurus.config.ts
+++ docs-site/docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://manjunathh-xyz.github.io/kexra',
+  url: 'https://manjunathh-xyz.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/kexra/',
